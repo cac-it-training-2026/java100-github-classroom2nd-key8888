b0839dea4ba191e86af16aafb0485a12972412b4
zicheng <zicheng@C010423383.zipangu.cac.co.jp>
2026-04-30 11:51:16 +0900

    lesson 09 challenge 01~04 完成しました

--- Java100_questions_cac2nd/src/lesson09/challenge03/Nurserry/logic/Nurse.java
+++ Java100_questions_cac2nd/src/lesson09/challenge03/Nurserry/logic/Nurse.java
@@ -1,25 +1,31 @@
 package lesson09.challenge03.Nurserry.logic;
 
 public class Nurse {
-    private String name;
+	private String name;
 
-    public Nurse(String name) {
-        this.name = name;
-    }
+	public Nurse(String name) {
+		this.name = name;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public void divide(int cakes, int childs) {
+	public void divide(int cakes, int childs) {
 
+		try {
+			int cakesPerChild = cakes / childs;
+			System.out.println("1人" + cakesPerChild + "個になります。");
+		} catch (ArithmeticException e) {
+			// : handle exception
+			System.out.println("ケーキを食べる園児がいません。");
+		} finally {
+			System.out.println("ケーキの分配が終わりました。");
+		}
 
-        //ここに適切な処理を記述
-
-
-    }
+	}
 }
